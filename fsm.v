b90a7ad65590ae11b3beb94fc2eb630521177589
Chad Coates <ccoates@nrgsim.com>
2017-10-10 05:36:01 -0700

    done

--- fsm.v
+++ fsm.v
@@ -1,3 +1,17 @@
+/*
+	Chad Coates
+	ECE 485
+	Homework 1
+	October 8, 2017
+
+	fsm.v
+
+	A fun little fsm for preventing 2 trains from
+	colliding on a shared piece of track
+
+	Adapted from the TBird example
+//*/
+
 module TrainState(SW,DA,DB,RESET,SR,Clock);
 
 input RESET, Clock;
@@ -10,17 +24,18 @@ reg [3:1] SW;
 reg [1:0] DA, DB;
 
 parameter
-	Ago   = 4'b0001,
-	Bgo   = 4'b0010,
-	Astop = 4'b0100,
-	Bstop = 4'b1000;
+	Init	= 5'b00001,
+	Ago   = 5'b00010,
+	Bgo   = 5'b00100,
+	Astop = 5'b01000,
+	Bstop = 5'b10000;
 
-reg [3:0] State, NextState;
+reg [4:0] State, NextState;
 
 always @(posedge Clock)
 begin
 	if (RESET) 
-		State <= Bgo;
+		State <= Init;
 	else
 		State <= NextState;
 end
@@ -28,12 +43,18 @@ end
 always @(State)
 begin
 case (State)
+	Init: begin // o_0
+		SW = 3'b011;
+		DA = 2'b01;
+		DB = 2'b01;
+		end
 	Ago: begin
 		SW = 3'b000;
 		DA = 2'b01;
 		DB = 2'b01;
 		end
-	Bgo: begin
+ // Init and Bgo have the same output
+	Bgo: begin // o_0
 		SW = 3'b011;
 		DA = 2'b01;
 		DB = 2'b01;
@@ -53,36 +74,40 @@ end
 
 always @(State or SR)//[4] or SR[3] or SR[1] or SR[2])
 begin
-case (State) 
-	Ago: begin
-		if (SR[2] && ~SR[4])
-			NextState = Bstop;
-		else if (SR[2] && SR[4])
-			NextState = Bgo;
-		end
-	Bgo: begin
-		if ((SR[1] && SR[2]) || (SR[1] && ~SR[2] && ~SR[3]))
-			NextState = Astop;
-		else if ((SR[1] && SR[3]) || (SR[1] && ~SR[2]))
-			NextState = Ago;
-		else if (SR[1] && ~SR[3])	
-			NextState = Bstop;
-		end
-	Astop: begin
-		if(SR[3])
-			NextState = Ago;
-		end
-	Bstop: begin
-		if(SR[4])
-			NextState = Bgo;
-		end
-/*
-	default:
-		begin
-			NextState = Astop;
-		end
-//*/
-endcase
+	case (State) 
+		Init: begin 
+			if (SR[1] && !SR[2])
+				NextState = Ago;
+			else if (SR[1] && SR[2])
+				NextState = Astop;
+			else if (!SR[1] && SR[2])	
+				NextState = Bgo;
+			end
+		Ago: begin
+			if (SR[1] && SR[3])
+				NextState = Bgo;
+			else if (SR[2] && !SR[4])
+				NextState = Bstop;
+			else if (SR[4])
+				NextState = Init;
+			end
+		Bgo: begin
+			if (SR[2] && SR[4])
+				NextState = Ago;
+			else if (SR[1] && !SR[3])
+				NextState = Astop;
+			else if (SR[3])
+				NextState = Init;
+			end
+		Astop: begin
+			if(SR[3])
+				NextState = Ago;
+			end
+		Bstop: begin
+			if(SR[4])
+				NextState = Bgo;
+			end
+	endcase
 end
 
 endmodule
--- tb.v
+++ tb.v
@@ -1,6 +1,22 @@
+/*
+	Chad Coates
+	ECE 485
+	Homework 1
+	October 8, 2017
+
+	tb.v
+
+	A fun little testbench to help crash imaginary trains
+	on an imaginary track! 
+	
+	Adapted from the TBird example
+//*/
+
 module TestTrainState;
+
 reg RESET,Clock;
 reg [4:1] SR;
+
 wire [3:1] SW;
 wire [1:0] DA, DB;
 
@@ -8,7 +24,7 @@ parameter TRUE   = 1'b1;
 parameter FALSE  = 1'b0;
 parameter CLOCK_TIMES  = 20;
 parameter CLOCK_WIDTH  = CLOCK_TIMES/2;
-parameter IDLE_CLOCKS  = 2;
+parameter IDLE_CLOCKS  = 1;
 parameter TIMES = 1;
 
 TrainState ts(SW,DA,DB,RESET,SR,Clock);
@@ -24,7 +40,7 @@ begin
 	forever #CLOCK_WIDTH Clock = ~Clock;
 end
 
-// Generate RESET signal for two cycles
+// Generate RESET signal for IDLE_CLOCKS cycle
 
 initial
 begin
@@ -34,63 +50,56 @@ begin
 end
 
 // Generate stimulus after waiting for reset
-
-reg [4:0] i;
 initial
 begin
-//*
+	// test all possible transitions
 	repeat (TIMES) @(negedge Clock);
-//	{RESET} = 1'b1;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b1000;repeat (TIMES) @(negedge Clock);
-
-	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b1010;repeat (TIMES) @(negedge Clock);
-
-	{SR} = 4'b0101;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b1010;repeat (TIMES) @(negedge Clock);
+	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock); // Init -> Ago
+	{SR} = 4'b1000;repeat (TIMES) @(negedge Clock); // Ago -> Init
+	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock); // Init -> Ago
 	
-	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b1010;repeat (TIMES) @(negedge Clock);
-//*/
+	{RESET} = 1'b1;repeat (TIMES) @(negedge Clock); // RESET
+	{RESET} = 1'b0;repeat (TIMES) @(negedge Clock); // RESET
+
+	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock); // Init -> Bgo
+	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock); // Bgo -> Init
+	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock); // Init -> Bgo
+	{SR} = 4'b1010;repeat (TIMES) @(negedge Clock); // Bgo -> Ago
+
+	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock); // Ago -> Bstop
+	{SR} = 4'b1000;repeat (TIMES) @(negedge Clock); // Bstop -> Bgo
+	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock); // Bgo -> Astop
+	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock); // Astop -> Ago
 	
-/*
-	repeat (TIMES) @(negedge Clock);
-//	{RESET} = 1'b1;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock);
-//	$display(" %b %b %b %b %b",RESET ,SR, SW, DA, DB);
-	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock);
-//	$display(" %b %b %b %b %b",RESET ,SR, SW, DA, DB);
-	{SR} = 4'b1010;repeat (TIMES) @(negedge Clock);
-//	$display(" %b %b %b %b %b",RESET ,SR, SW, DA, DB);
-//*/
-/*
-	repeat (TIMES) @(negedge Clock);
-	for(i=5'b0_0000;i<5'b1_0000;i=i+1'b1)
-	begin
-	{SR} = i[3:0];repeat (TIMES) @(negedge Clock);
-	end
-//*/
-/*
-	repeat (TIMES) @(negedge Clock);
-//	{RESET} = 1'b1;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0000;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0011;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0101;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0110;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0111;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0101;repeat (TIMES) @(negedge Clock);
-	{SR} = 4'b0111;repeat (TIMES) @(negedge Clock);
-//*/
-//	$stop;
+	{SR} = 4'b0101;repeat (TIMES) @(negedge Clock); // Ago -> Bgo
+	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock); // Bgo -> Init
+	
+	{SR} = 4'b0011;repeat (TIMES) @(negedge Clock); // Init -> Astop
+	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock); // Astop -> Ago
+	{SR} = 4'b1000;repeat (TIMES) @(negedge Clock); // Ago -> Init
 	$finish;
 end
 
 endmodule
+/*
+	transitions
+	{RESET} = 1'b1;repeat (TIMES) @(negedge Clock); // RESET
+
+	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock); // Init -> Ago
+	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock); // Init -> Bgo
+	{SR} = 4'b0011;repeat (TIMES) @(negedge Clock); // Init -> Astop
+
+	{SR} = 4'b1000;repeat (TIMES) @(negedge Clock); // Ago -> Init
+	{SR} = 4'b0010;repeat (TIMES) @(negedge Clock); // Ago -> Bstop
+	{SR} = 4'b0101;repeat (TIMES) @(negedge Clock); // Ago -> Bgo
+
+	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock); // Bgo -> Init
+	{SR} = 4'b0001;repeat (TIMES) @(negedge Clock); // Bgo -> Astop
+	{SR} = 4'b1010;repeat (TIMES) @(negedge Clock); // Bgo -> Ago
+
+	{SR} = 4'b1000;repeat (TIMES) @(negedge Clock); // Bstop -> Bgo
+	
+	{SR} = 4'b0100;repeat (TIMES) @(negedge Clock); // Astop -> Ago
+//*/
+
+
